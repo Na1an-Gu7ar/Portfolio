b939a281c847034ca0430f28805851a264cc71ed
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-03-11 23:41:25 +0530

    MyWorkAdded

---
+++ src/components/MyWork/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
+import {useState, useEffect} from 'react'
+
+const MyWork = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['M', 'y', '', 'W', 'o', 'r', 'k']}
+                            idx={15}
+                        />
+                    </h1>
+                </div>
+            </div>
+            <Loader type="line-scale-pulse-out" />
+        </>
+    )
+}
+
+export default MyWork
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoN from '../../assets/images/logo-N.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faClose, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClose, faEnvelope, faHome, faUser, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
@@ -22,6 +22,9 @@ const Sidebar = () => {
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
                         <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                    </NavLink>
+                    <NavLink exact="true" activeclassname="active" className="mywork-link" to="/mywork" onClick={() => setShowNav(false)}>
+                        <FontAwesomeIcon icon={faBriefcase} color='#4d4d4e' />
                     </NavLink>
                     <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
                         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
