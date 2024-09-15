386ba4e2ecda08c72daee7e6c73d9e6b34a60f4d
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-09-16 00:24:06 +0530

    abcde

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,7 +7,7 @@ import LogoN from '../../assets/images/logo-N.png'
 import Logo from './Logo'
 import Loader from 'react-loaders'
 import 'ldrs/zoomies'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -21,34 +21,37 @@ const Home = () => {
     }, [])
 
     return (
-        <motion.div 
-        initial={{y: "100%"}}
-        animate={{y: "0%"}}
-        transition={{duration: 0.5, ease: "easeOut", type: "spring"}}
-        exit={{opacity: 1}}
-        >
-            <div className='container home-page'>
-                <div className='text-zone'>
-                    <h1>
-                        <span className={`${letterClass}_11`}>H</span>
-                        <span className={`${letterClass} _12`}>i,</span>
-                        <br />
-                        <span className={`${letterClass} _13`}>I</span>
-                        <span className={`${letterClass} _14`}>'m</span>
-                        {/* <span className='N-logo'>N</span> */}
-                        <img src={LogoN} alt="nayan" />
-                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
-                        <br />
-                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
-                    </h1>
-                    <h2>FullStack Developer / Ethical Hacker</h2>
-                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <AnimatePresence>
+
+            <motion.div
+                initial={{ y: "100%" }}
+                animate={{ y: "0%" }}
+                transition={{ duration: 0.5, ease: "easeOut", type: "spring" }}
+                exit={{ opacity: 1 }}
+            >
+                <div className='container home-page'>
+                    <div className='text-zone'>
+                        <h1>
+                            <span className={`${letterClass}_11`}>H</span>
+                            <span className={`${letterClass} _12`}>i,</span>
+                            <br />
+                            <span className={`${letterClass} _13`}>I</span>
+                            <span className={`${letterClass} _14`}>'m</span>
+                            {/* <span className='N-logo'>N</span> */}
+                            <img src={LogoN} alt="nayan" />
+                            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                            <br />
+                            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
+                        </h1>
+                        <h2>FullStack Developer / Ethical Hacker</h2>
+                        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                    </div>
+                    <Logo />
                 </div>
-                <Logo />
-            </div>
-            {/* <Loader type='line-scale-pulse-out' /> */}
-            {/* <l-zoomies size={80} stroke={3} speed={1.5} color="#fdd700" bg-opacity={0.2}/> */}
-        </motion.div>
+                {/* <Loader type='line-scale-pulse-out' /> */}
+                {/* <l-zoomies size={80} stroke={3} speed={1.5} color="#fdd700" bg-opacity={0.2}/> */}
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
